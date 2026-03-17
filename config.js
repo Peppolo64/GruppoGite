0efa8b28711366493d19967ea9e1a5dbe0a444ba
Peppolo64 <159889605+Peppolo64@users.noreply.github.com>
2026-03-17 03:11:21 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,56 @@
+const CONFIG = {
+  "titolo": "Dentro il Bosco della Ficuzza",
+  "data": "Sabato 21 marzo 2026",
+  "anno": "2026",
+  "organizzatore": "Gruppo Insoliti Montanari",
+  "scadenza_adesione": "giovedì 20 marzo",
+  "difficolta_link": "https://drive.google.com/file/d/1Z4SUhEkIMUEJZqId27PsAwxymrc_8qun/view?usp=drive_link",
+
+  "punto_incontro": {
+    "nome": "Bar Eni Station Bonagia",
+    "citta": "Palermo",
+    "ora_ritrovo": "8:30",
+    "ora_partenza": "9:00",
+    "durata_trasferimento": "~55 min (60 km)",
+    "maps_link": "https://maps.app.goo.gl/7paSbYKRgPBX8Rp58",
+    "embed_url": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2190.390875680228!2d13.3642298!3d38.0879172!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1319e57a56ebcd0b%3A0x2d868dd0c560bd41!2sche%20Bont%C3%A0%20BAR%20%7C%20Torre%20Bonagia%201!5e1!3m2!1sit!2sit!4v1773707565394!5m2!1sit!2sit"
+  },
+
+  "trekking": {
+    "ora_inizio": "10:30",
+    "tipo": "Anello",
+    "km": "6,6",
+    "dislivello_m": "200",
+    "durata_stimata": "~2h 20min",
+    "quota_max_m": "880",
+    "difficolta": "Facile",
+    "difficolta_livello": 2,
+    "osmand_link": "https://osmand.net/map?start=37.882313%2C13.378472&via=37.885872%2C13.396818%3B37.874825%2C13.397576&end=37.882298%2C13.378391&profile=pedestrian#15/37.883476/13.389877"
+  },
+
+  "parcheggio": {
+    "nome": "Ficuzza",
+    "maps_link": "https://maps.app.goo.gl/PQuzuH6vvULXAkYt9",
+    "embed_url": "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2292.073133664365!2d13.377486!3d37.882795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzfCsDUyJzU4LjEiTiAxM8KwMjInMzkuMCJF!5e1!3m2!1sit!2sit!4v1773706423639!5m2!1sit!2sit"
+
+  },
+
+  "meteo": [
+    { "label": "3BMeteo – Marineo",   "embed_url": "https://www.3bmeteo.com/widget-meteo/marineo",  "link": "https://www.3bmeteo.com/meteo/marineo"  },
+    { "label": "3BMeteo – Corleone",  "embed_url": "https://www.3bmeteo.com/widget-meteo/corleone", "link": "https://www.3bmeteo.com/meteo/corleone" },
+    { "label": "Il Meteo – Marineo",  "embed_url": "https://www.ilmeteo.it/meteo/marineo",          "link": "https://www.ilmeteo.it/meteo/marineo"   },
+    { "label": "Il Meteo – Corleone", "embed_url": "https://www.ilmeteo.it/meteo/corleone",         "link": "https://www.ilmeteo.it/meteo/corleone"  }
+  ],
+
+  "google_form_embed": "https://docs.google.com/forms/d/e/1FAIpQLSeqvYSl9KOoBUKHxT9jzLJ4gfxTdPe1yOYk_D5xZS6rRqOJQA/viewform?usp=header",
+
+  "equipaggiamento": [
+    "Scarpe da trekking",
+    "Abbigliamento tecnico adatto alla stagione",
+    "Giacca antivento",
+    "Strato caldo (pile/piumino/felpa)",
+    "Acqua ≥ 1,5 L/persona",
+    "Merenda + pranzo al sacco (a carico di ciascun partecipante)",
+    "Cambio completo da lasciare in auto"
+  ],
+};
